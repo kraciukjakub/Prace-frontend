e96df5b2c15b9200cd07d85949cbf16e0fce1609
kraciukjakub <141629633+kraciukjakub@users.noreply.github.com>
2023-08-07 13:56:21 +0200

    Add files via upload

---
+++ gierka/game.js
@@ -0,0 +1,25 @@
+const gameTiles = document.querySelectorAll(".tile");
+const gameBoard = document.querySelector("#game-board");
+
+const gameState = [
+  [gameTiles[0], gameTiles[1], gameTiles[2]],
+  [gameTiles[3], gameTiles[4], gameTiles[5]],
+  [gameTiles[6], gameTiles[7], gameTiles[8]],
+];
+
+gameBoard.addEventListener("click", (event) => {
+  const target = event.target;
+
+  let x, y;
+
+  gameState.forEach((row, rowIndex) => {
+    row.forEach((column, columnIndex) => {
+      if (column === target) {
+        x = rowIndex;
+        y = columnIndex;
+      }
+    });
+  });
+
+  console.log(x, y);
+});
